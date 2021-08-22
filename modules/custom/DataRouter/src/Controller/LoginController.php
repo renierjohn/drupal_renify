4fa0c2fe11419cd5fa986f73d3519609141d20c5
Rendroid <renierjohnsediego@gmail.com>
2021-08-22 14:00:49 +0800

    add custom module

---
+++ modules/custom/DataRouter/src/Controller/LoginController.php
@@ -0,0 +1,189 @@
+<?php
+
+namespace Drupal\data_router\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+use Drupal\Core\Render\HtmlResponse;
+use Drupal\data_router\Service\AccountService;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\HttpFoundation\RequestStack;
+use GuzzleHttp\Exception\ClientException;
+use GuzzleHttp\Client;
+/**
+ * Class ImceAdminBrowserController.
+ */
+class LoginController extends ControllerBase {
+
+  protected $account;
+
+  protected $request;
+
+  protected $client;
+
+  const ENDPOINT = 'https://oauth2.googleapis.com/token';
+
+  const RESPONSE_TYPE = 'token';
+
+  const REDIRECT_URL  = 'https://renify.store/google';
+
+  const CLIENT_SECRET = 'oAAA-lDtg7LqyrsjS5rh5zsq';
+
+  const CLIENT_ID     = '902176944767-2ul1lh81t998833bmnsjah6sg2k9e9p2.apps.googleusercontent.com';
+
+  const SCOPE         = 'https://www.googleapis.com/auth/userinfo.profile.email';
+
+  public function __construct(AccountService $account,RequestStack $request,Client $client) {
+    $this->account = $account;
+    $this->request = $request->getCurrentRequest();
+    $this->client  = $client;
+  }
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container) {
+    return new static(
+      $container->get('data_router.account'),
+      $container->get('request_stack'),
+      $container->get('http_client')
+    );
+  }
+
+  /**
+   * Browser Page.
+   *
+   * @return string
+   *   Return Hello string.
+   */
+  public function verify($token) {
+    $name = $this->account->setToken($token)->checkToken();
+     if(empty($name)){
+       \Drupal::messenger()->addError('Invalid Link');
+     }
+     else{
+       \Drupal::messenger()->addMessage('Welcome '.$name);
+     }
+
+     $redirect =  new RedirectResponse('/');
+     return $redirect->send();
+  }
+
+  public function google(){
+    $query   = $this->request->query->all();
+    $code    = $query['code'];
+    $account = $this->account;
+    
+    $result = [];
+    $token  = FALSE;
+
+    $uri  = 'https://oauth2.googleapis.com/token';
+
+    $client_id     = self::CLIENT_ID;
+    $client_secret = self::CLIENT_SECRET;
+    $redirect_uri  = self::REDIRECT_URL;
+    $scope         = self::SCOPE;
+
+    $data = [
+      'code'          => $code,
+      'client_id'     => $client_id,
+      'client_secret' => $client_secret,
+      'redirect_uri'  => $redirect_uri,
+      'grant_type'    => 'authorization_code',     
+    ];
+
+    try {
+      $result =  \Drupal::httpClient()->post($uri,[ // get access_token
+        'form_params'=> $data,
+        'verify'     => TRUE,
+        'headers' => [
+          'Content-type' => 'application/x-www-form-urlencoded',
+        ],
+      ]);
+      
+      $result  = $result->getBody()->getContents();
+      $result  = json_decode($result,TRUE);
+      $token   = $result['access_token']; 
+    }
+    catch(ClientException $e){
+      \Drupal::messenger()->addError('Something went wrong . Please Try Again');
+         return new RedirectResponse('/user/login');
+    } 
+    catch (Exception $e) {
+       \Drupal::messenger()->addError('Something went wrong . Please Try Again');
+         return new RedirectResponse('/user/login');
+    }
+
+    if(!empty($token)){
+      try {
+        $uri     = 'https://www.googleapis.com/oauth2/v3/userinfo?access_token='.$token;
+        $result  = \Drupal::httpClient()->get($uri); // get profile info
+        $result  = $result->getBody()->getContents();
+        $result  = json_decode($result,TRUE);
+      }
+      catch(ClientException $e){
+        \Drupal::messenger()->addError('Something went wrong . Please Try Again');
+         return new RedirectResponse('/user/login');
+      } 
+      catch (Exception $e) {
+        \Drupal::messenger()->addError('Something went wrong . Please Try Again');
+         return new RedirectResponse('/user/login');
+      }
+    }
+
+    if(empty($result)){
+         \Drupal::messenger()->addError('Something went wrong . Please Try Again');
+         return new RedirectResponse('/user/login');
+    }
+
+    $email   = $result['email'];
+    $profile = $result['picture'];
+    $name    = $result['name'].' '.$result['given_name'];
+
+    // return new JsonResponse(
+    //   [
+    //     'code'   => $code,
+    //     'token'  => $token,
+    //     'result' => $result,
+    //     'email'   => $email,
+    //     'profile' => $profile,
+    //     'name'    => $name,
+    //   ]);
+
+
+    $user_result = $account->checkAccount($email);
+    if(empty($user_result)){
+      $profile = system_retrieve_file($profile, 'public://google', FALSE, FILE_EXISTS_REPLACE);
+      $user    = $account->registerDirect($name,$email,$profile);
+      user_login_finalize($user);
+      \Drupal::messenger()->addMessage('Success . Welcome '.$email);
+      return new RedirectResponse('/');
+    }
+
+    user_login_finalize($user_result);
+    \Drupal::messenger()->addMessage('Success . Welcome '.$email);
+    return new RedirectResponse('/');
+
+  }
+
+}
+    // $content = 
+    // '<html class="no-js" lang="en">
+    //   <head>
+    //     <link rel="manifest" href="manifest.json" />
+    //     <meta name="author" content="Renify">
+    //     <title>Google | Renify</title>
+    //   </head>
+    //   <body>
+    //   <h5>Authenticatings...</h5>
+    //   <input type="hidden" value='.$code.' />
+    //   <script src="/core/assets/vendor/jquery/jquery.min.js?v=3.6.0"></script>
+    //   <script src="/themes/Renify/js/google.js"></script>
+    //  </body>
+    // </html>';
+    // \Drupal::service('page_cache_kill_switch')->trigger();
+    // $response =  new HtmlResponse($content,200,['Cache-Control'=>'no-cache','X-Drupal-Dynamic-Cache'=>'UNCACHEABLE ']);
+    // $response->mustRevalidate();
+    // $response->setMaxAge(0);
+
+    // return $response;
